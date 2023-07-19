d2ff04872abb5e55c206b1f4b88554c2c05860cb
goharabbas007 <42521533+goharabbas007@users.noreply.github.com>
2023-07-19 23:00:35 +0500

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   e2e: {
     env:{
         baseUrl: 'https://gorest.co.in/public/v2',
-        token: 'Bearer 436aa09186c1f54f23d3facb6c6a6645933127a58ed95f08cf54952381ecfe1f'
+        token: 'Bearer youwilluseyourbearertokenhereinsteadofthistext'
     },
     watchForFileChanges: false,
     defaultCommandTimeout: 6000,
